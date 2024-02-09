35b0369d84722ab84bd8d62ea7b632f4764cbf95
chancellorofpaphos <chancellorofpaphos@protonmail.com>
2024-02-09 09:50:47 +0000

    Added script for flipping video horizontally.

---
+++ useful_scripts/ffmpeg/hflip.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+### This script cuts a given video file.
+
+# Constants.
+PATH_TO_INPUT="input.mp4"
+PATH_TO_OUTPUT="output.mp4"
+START="00:00:00"
+STOP="00:00:00"
+
+# Let's get cracking...
+ffmpeg -i $PATH_TO_INPUT -vf hflip -c:a copy $PATH_TO_OUTPUT
